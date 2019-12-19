db525503791aa1757fed1e2ae4e291f5ba880ba8
Sanchit <Sanchitraina1999@gmail.com>
2019-12-19 11:22:56 +0530

    Binary Tree

---
+++ 01BinaryTree/src/01BinaryTree.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : 01BinaryTree.cpp
+// Author      : Sanchit Raina
+// Version     :
+// Copyright   : https://www.github.com/Sanchitraina1999/ADSL
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/*
+ Given binary tree with n nodes and perform following operations on
+it.
+a) Assign this tree to another [operator=]
+b) Erase all nodes in a binary tree
+c) Create a mirror image of the tree
+d) Check two binary trees are equal or not
+e) Inorder, Preorder, Postorder traversal of tree(recursive and
+non-recursive)
+f) Print internal and leaf nodes
+ */
+
+
+#include <iostream>
+using namespace std;
+
+class node{
+
+};
+
+class list{
+
+
+};
+int main() {
+
+	return 0;
+}
